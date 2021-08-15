d8235bcd93b90902f77909b21faa3b7cc9b4b6e4
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-08-15 17:41:28 -0300

    Escolhendo a Ação

---
+++ exercicios/ex010/script.js
@@ -0,0 +1,18 @@
+let msg = document.querySelector('div#msg')
+
+function acao1(){
+    msg.innerHTML += `<p>Clicou no <strong>primeiro botão</strong></p>`
+}
+
+function acao2(){
+    msg.innerHTML += `<p>Clicou no <strong>segundo botão</strong></p>`
+}
+
+function acao3(){
+    msg.innerHTML += `<p>Clicou no <strong>terceiro botão</strong></p>`
+    
+}
+
+function acao4(){
+    msg.innerHTML += `<p>Clicou no <strong>quarto botão</strong></p>`
+}
