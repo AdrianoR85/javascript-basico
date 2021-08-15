9deeb72ecbbc069c44d6b4a0a51f8276792348a9
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-08-15 14:19:11 -0300

    Tranabalhando com dados

--- exercicios/ex004/script.js
+++ exercicios/ex004/script.js
@@ -2,6 +2,6 @@ function clicar(){
     var msg = document.querySelector('div#msg')
     var nome = prompt("What's your name?")
 
-    msg.innerHTML = `Hi, ${nome}! Nice to meet you.`
+    msg.innerHTML = `Hi, <strong>${nome}</strong>! Nice to meet you! &#x1F596;`
 
 }
