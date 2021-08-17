948684df7ac046b3743058d74d78dd45a5fc2aa5
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-08-17 06:28:26 -0300

    Classe Date()

---
+++ exercicios/ex014/script.js
@@ -0,0 +1,7 @@
+function start(){
+    let msg = document.querySelector('div#msg')
+    let data = new Date()
+
+    msg.innerHTML = `<strong>Dados recebidos do sistema:</strong><br>${data} `
+    
+}
