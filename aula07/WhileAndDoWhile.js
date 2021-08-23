588acdbd12a1f98280adb3fc2275989a9799d445
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-08-23 06:41:32 -0300

    Estrutura While e Do While

---
+++ aula07/WhileAndDoWhile.js
@@ -0,0 +1,15 @@
+
+let cont = 1
+do{
+    console.log(`Passo ${cont}`)
+    cont++
+} while( cont < 10)
+
+
+/* Estrutura While
+let cont = 1
+while(cont < 6){
+    console.log(`Passo ${cont}`)
+    cont++
+}
+*/
