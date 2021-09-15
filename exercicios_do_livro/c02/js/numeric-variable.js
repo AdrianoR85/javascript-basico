a71cf22babe3f6f90637d7219e35bb30fe04942f
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-09-15 06:55:27 -0300

    Variables

---
+++ exercicios_do_livro/c02/js/numeric-variable.js
@@ -0,0 +1,7 @@
+let price = 5;
+let quantity = 14;
+let total = price * quantity;
+
+let el = document.getElementById('cost');
+el.textContent = '$' + total
+
---
+++ exercicios_do_livro/c02/js/string-variable.js
@@ -0,0 +1,7 @@
+let username = 'Molly';
+let menssage = 'See our upcoming range';
+
+let elName = document.getElementById('name');
+elName.textContent = username;
+let elNote = document.getElementById('note');
+elNote.textContent = menssage;
