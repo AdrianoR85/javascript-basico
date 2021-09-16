7da8b7c0a1d1328d692959d2a26caedbc56fd482
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-09-16 06:12:57 -0300

    String cm aspas

---
+++ exercicios_do_livro/c02/js/string-with-quotes.js
@@ -0,0 +1,7 @@
+let title = "Molly's Special Offers";
+let menssage =  '<a href=\"sale.html\">25% off!</a>';
+
+let elTitle = document.getElementById('title');
+elTitle.textContent = title;
+let elNote = document.getElementById('note');
+elNote.innerHTML = menssage;
