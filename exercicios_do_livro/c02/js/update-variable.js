ef6107a020adad9ca9000b1c1b361a66a7c5819b
ADRIANO ROSA <adrianorosa085@gmail.com>
2021-09-18 06:25:15 -0300

    Update variable

---
+++ exercicios_do_livro/c02/js/update-variable.js
@@ -0,0 +1,11 @@
+let inStock;
+var shipping;
+
+let elStock = document.getElementById('stock');
+let elShip = document.getElementById('shipping'); 
+
+inStock = false;
+shipping = true;
+
+elStock.className = inStock;
+elShip.className = shipping;
